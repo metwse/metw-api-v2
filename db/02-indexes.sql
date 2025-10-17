775e3577643cb4865a620909126ba1f6eeb0786f
Metehan Selvi <me@metehanselvi.com>
2025-10-17 16:43:32 +0300

    feat!: id column in relations

--- db/02-indexes.sql
+++ db/02-indexes.sql
@@ -4,3 +4,18 @@ CREATE INDEX IF NOT EXISTS users_username_id_idx
     (username COLLATE pg_catalog."default" ASC NULLS LAST)
     INCLUDE(id)
     WITH (fillfactor=100, deduplicate_items=True);
+
+-- Index to speed up post lookups by thread_id
+CREATE INDEX IF NOT EXISTS posts_thread_id_id_idx
+    ON posts USING btree
+    (thread_id ASC NULLS FIRST)
+    INCLUDE(id)
+    WITH (fillfactor=100, deduplicate_items=True);
+
+-- Index to speed up post lookups by replies_thread_id
+CREATE INDEX IF NOT EXISTS posts_replies_thread_id_id_idx
+    ON public.posts USING btree
+    (replies_thread_id ASC NULLS LAST)
+    INCLUDE(id)
+    WITH (fillfactor=100, deduplicate_items=True);
+
--- db/03-relations.sql
+++ db/03-relations.sql
@@ -5,8 +5,10 @@ CREATE SCHEMA IF NOT EXISTS relations;
 -- ------------------------------------------------
 CREATE TABLE IF NOT EXISTS relations.likes
 (
+    id bigint NOT NULL,
     user_id bigint NOT NULL,
     post_id bigint NOT NULL,
+    CONSTRAINT likes_id_pkey PRIMARY KEY (id),
     CONSTRAINT likes_post_id_fkey FOREIGN KEY (post_id)
         REFERENCES posts (id) MATCH SIMPLE
         ON UPDATE NO ACTION
@@ -34,8 +36,10 @@ CREATE INDEX IF NOT EXISTS likes_user_id_post_id_idx
 -- ------------------------------------------------
 CREATE TABLE IF NOT EXISTS relations.follows
 (
+    id bigint NOT NULL,
     follower_id bigint NOT NULL,
     user_id bigint NOT NULL,
+    CONSTRAINT follows_id_pkey PRIMARY KEY (id),
     CONSTRAINT follows_follower_id_fkey FOREIGN KEY (follower_id)
         REFERENCES users (id) MATCH SIMPLE
         ON UPDATE NO ACTION
